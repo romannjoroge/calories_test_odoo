01e511f2d62e8668a0b34c304c08f2fd1d37c42a
Roman Njoroge <roman.njoroge@njuguna.com>
2026-07-02 16:15:07 +0300

    feat: Defining access to calorie users model

--- __manifest__.py
+++ __manifest__.py
@@ -4,6 +4,10 @@
     "version": "19.0.0.0.0",
     "license": "OEEL-1",
     "depends": ["base"],
-    "data": ["security/res_groups.xml"],
+    "data": [
+        # Security
+        "security/res_groups.xml",
+        "security/ir.model.access.csv"
+    ],
     "demo": ["demo/data.xml"]
 }
