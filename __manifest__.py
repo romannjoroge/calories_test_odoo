94e7753a0666d3ef9211da55e7a134afc172913c
Roman Njoroge <roman.njoroge@njuguna.com>
2026-07-02 15:26:41 +0300

    build: Exporing models

---
+++ __manifest__.py
@@ -0,0 +1,8 @@
+{
+    "name": "test_calories_app",
+    "summary": "Test application for recommending number of calories someone should take",
+    "version": "19.0.0.0.0",
+    "license": "OEEL-1",
+    "depends": ["base"],
+    "data": []
+}
